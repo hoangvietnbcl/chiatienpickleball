390705490ec844546a44e9e896d2dc97ce091d4e
hoangvietnbcl <hoangviet.nbcl@gmail.com>
2026-08-17 13:57:09 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,31 @@
+// Firebase Configuration
+// Hãy thay thế các giá trị dưới đây bằng cấu hình Firebase của bạn
+
+function getFirebaseConfig() {
+  const config = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+  };
+
+  return config;
+}
+
+// Hoặc để bảo mật, lưu config trong file .env và tải từ server
+// function getFirebaseConfig() {
+//   return {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.FIREBASE_DATABASE_URL,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID,
+//     measurementId: process.env.FIREBASE_MEASUREMENT_ID
+//   };
+// }
